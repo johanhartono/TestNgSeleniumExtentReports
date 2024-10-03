9c79770ee83df3623e7f3444cbe860c5f13737c9
johanhartono <johanhartono@gmail.com>
2024-10-03 11:51:59 +0700

    update

--- src/setup/utils.java
+++ src/setup/utils.java
@@ -4,5 +4,5 @@ public class utils {
 	public static String webUserName = "Admin";
 	public static String webPassword = "admin123";
 	public static String releaseNumber = "22";
-	public static String webUrl ="https://opensource-demo.orangehrmlive.com";
+	public static String webUrl ="use your own web. do not user any web that unauthorized/not permitted.";
 }
