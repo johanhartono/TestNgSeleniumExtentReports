d6ec90464e1af9eb60e0c9d73721a440eced6991
JH <johanhartono@gmail.com>
2024-10-13 22:26:20 +0700

    Delete src/setup/utils.java

--- src/setup/utils.java
+++
@@ -1,8 +0,0 @@
-package setup;
-
-public class utils {
-	public static String webUserName = "";
-	public static String webPassword = "";
-	public static String releaseNumber = "22";
-	public static String webUrl ="";
-}
