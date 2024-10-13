372708ab33d5a499e0120b2f6e7099ee63405f32
JH <johanhartono@gmail.com>
2024-10-13 22:25:22 +0700

    Update utils.java

--- src/setup/utils.java
+++ src/setup/utils.java
@@ -1,8 +1,8 @@
 package setup;
 
 public class utils {
-	public static String webUserName = "admin";
-	public static String webPassword = "Jolin@2021";
+	public static String webUserName = "";
+	public static String webPassword = "";
 	public static String releaseNumber = "22";
-	public static String webUrl ="https://johanhartono.my.id/OrangeHR";
+	public static String webUrl ="";
 }
