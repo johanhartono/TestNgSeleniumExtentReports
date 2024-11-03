18d728908bf022f783c6409554328ca07d50da7a
johanhartono <johanhartono@gmail.com>
2024-11-03 21:27:30 +0700

    update

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -2,12 +2,10 @@ package pages;
 
 import java.time.Duration;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
--- src/testcases/LoginTests.java
+++ src/testcases/LoginTests.java
@@ -2,63 +2,20 @@
 
 package testcases;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
-import setup.utils;
 
 import pages.LoginPage;
 import setup.BaseTests;
+import setup.utils;
 
 public class LoginTests extends BaseTests {
 
 	LoginPage objLogin;
-	String exptectedTitle = "OrangeHRM";
-	String actualTitle;
-	String expectedCopyRight = "OrangeHRM OS 5.7";
 
-	@Test(priority = 0, enabled = true, description = "get Page Title")
-	public void openWeb() throws InterruptedException {
-		objLogin = new LoginPage(BaseTests.driver);
-		Thread.sleep(5000);
-		actualTitle = BaseTests.driver.getTitle();
-		Assert.assertEquals(actualTitle, exptectedTitle);
-	}
-
-	@Test(priority = 1, enabled = true, description = "Validate Login Page Title Label")
-	public void verifyLoginPageTitle() throws Throwable {
-		Thread.sleep(1000);
-		String loginPageTitle = objLogin.getLoginTitle();
-		Assert.assertTrue(loginPageTitle.contains("Login"));
-	}
-
-	@Test(priority = 2, enabled = true, description = "Validate Login Page Copyright Label")
-	public void verifyCoyRight() throws Throwable {
-		Thread.sleep(1000);
-		String CopyRight = objLogin.getCopyRight();
-		Assert.assertTrue(CopyRight.contains(expectedCopyRight));
-	}
-
-	@Test(priority = 3, enabled = false)
-	public void verifyforgetPasswordLink() {
-
-		// String expectedText= objLogin.getforgotPasswordLinkText();
-		// Assert.assertTrue(expectedText.contains("Forgot your password?"));
-
-	}
-	
-	@Test(priority = 4, enabled = false)
-	public void verifyLinkedIn() {
-
-		// System.out.println("Actual linkedIn Text :" + objLogin.isEnabledLinkedIn());
-		// assertTrue(objLogin.isEnabledLinkedIn());
-
-		// System.out.println("Im in skip exception");
-		// throw new SkipException("Skipping this exception");
-	}
-	
-	@Test(priority = 5, enabled = true, description = "Login using valid credentials username and password")
+	@Test(priority = 1, enabled = true, description = "Login using valid credentials username and password")
 	public void LoginTest() {
 		objLogin.login(utils.webUserName, utils.webPassword);
+		objLogin.clickLogin();
 	}
 
 
